7a5960467058db7e2dd72e6e16092c368c01b019
0x142857 <0x142857@gmail.com>
2015-02-16 17:24:08 +0800

    update

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 var request = require('request')
 	,	cheerio = require('cheerio')
-	
-request.get('http://jandan.net/ooxx', function (error, response, body) {
+
+var argv = process.argv.slice(2)
+  , url = 'http://jandan.net/ooxx'
+
+if(argv[0]) {
+  console.log('fetching page ' + argv[0])
+  url += '/page-' + argv[0]
+}
+
+
+request.get(url, function (error, response, body) {
+  if (error) {
+    console.log('url not exists')
+    return
+  }
 	var $ = cheerio.load(body)
 	$('.commentlist').find('img').each(function () {
 		var source = $(this).attr('src')
